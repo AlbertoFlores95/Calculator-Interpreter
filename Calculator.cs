2414a7580d8df16212ae9705d0722f68b9a28da0
Alberto Flores Sanchez <Alberto@Flores.cf>
2016-10-16 14:05:58 -0600

    Boolean evaluator

--- Calculator.cs
+++ Calculator.cs
@@ -31,106 +31,182 @@ public class Calculator {
         return token;
     }
 
-	public double Power() {
+	public Protuberance Power() {
         MatchAndEat("POWER");
-        return Term();
+        return Factor();
     }
 	
-	public double Mod() {
+	public Protuberance Mod() {
         MatchAndEat("MOD");
-        return Factor();
+        return AlmostFactor();
     }
 	
-    public double Multiply() {
+    public Protuberance Multiply() {
         MatchAndEat("MULTIPLY");
-        return Factor();
+        return AlmostFactor();
     }
 
-    public double Divide() {
+    public Protuberance Divide() {
         MatchAndEat("DIVIDE");
-        return Factor();
+        return AlmostFactor();
     }
 
-    public double Add() {
+    public Protuberance Add() {
         MatchAndEat("ADD");
         return Term();
     }
 
-    public double Subtract() {
+    public Protuberance Subtract() {
         MatchAndEat("SUBTRACT");
         return Term();
     }
 
-    public double Factor() {
-        double result = 0;
+    public Protuberance Factor() {
+        Protuberance result = null;
         if (CurrentToken().type.Equals("LEFT_PAREN")) {
             MatchAndEat("LEFT_PAREN");
-            result = ArithmeticExpression();
+            result = v8Expression();
             MatchAndEat("RIGHT_PAREN");
         } else if (CurrentToken().type.Equals("NUMBER")) {
-            result = Double.Parse(CurrentToken().text);
-            MatchAndEat("NUMBER");
+           Token token = MatchAndEat("NUMBER");
+		   double res=0;
+		   try{
+		   res = Double.Parse(token.text);
+		   }catch(FormatException){Console.WriteLine("Auiiiiii");}
+		   result = new Number(res);
+		   try{Console.WriteLine(result.opinion());}catch(NullReferenceException){Console.WriteLine("Factor");}
         }
         return result;
     }
 	
-	public double AlmostFactor() {
-        double result = Factor();
+	public Protuberance AlmostFactor() {
+        Protuberance result = Factor();
         while (CurrentToken().type.Equals("POWER")) {
             switch (CurrentToken().type) {
                 case "POWER":
-                    result = Math.Pow(result,Power());
+                    result = new Operation("POWER",result,Power());
                     break;
             }
         }
+		try{Console.WriteLine(result.opinion());}catch(NullReferenceException){Console.WriteLine("AF");}
         return result;
     }
 
-    public double Term() {
-        double result = AlmostFactor();
+    public Protuberance Term() {
+        Protuberance result = AlmostFactor();
         while (CurrentToken().type.Equals("MULTIPLY") || CurrentToken().type.Equals("DIVIDE")|| CurrentToken().type.Equals("MOD")) {
             switch (CurrentToken().type) {
                 case "MULTIPLY":
-                    result = result * Multiply();
+                    result = new Operation("MULTIPLY",result,Multiply());
                     break;
                 case "DIVIDE":
-                    result = result / Divide();
+                    result = new Operation("DIVIDE",result,Divide());
                     break;
 				case "MOD":
-                    result = result % Mod();
+                    result = new Operation("MOD",result,Mod());
                     break;
             }
         }
+		try{Console.WriteLine(result.opinion());}catch(NullReferenceException){Console.WriteLine("Term");}
         return result;
     }
 
-    public double ArithmeticExpression() {
-        double result = Term();
+    public Protuberance ArithmeticExpression() {
+        Protuberance result = Term();
         while (CurrentToken().type.Equals("ADD") || CurrentToken().type.Equals("SUBTRACT")) {
             switch (CurrentToken().type) {
                 case "ADD":
-                    result = result + Add();
+                    result = new Operation("ADD",result,Add());
+					try{Console.WriteLine(result.opinion());}catch(NullReferenceException){Console.WriteLine("AE");}
                     break;
                 case "SUBTRACT":
-                    result = result - Subtract();
+                    result = new Operation("SUBTRACT",result,Subtract());
                     break;
             }
         }
         return result;
     }
+	
+	public Protuberance Relation(){
+		Protuberance result = ArithmeticExpression();
+		//Protuberance result = null;
+		String type = CurrentToken().type;
+		//if(type.Equals("EQUAL")||type.Equals("LESS")||type.Equals("GREATER")||type.Equals("LESSEQUAL")||type.Equals("GREATEREQUAL")){
+			switch(type){
+				case "EQUAL": result = Equal(result);
+								//Console.WriteLine(result.ToString());
+								break;
+				case "LESS": result = Less(result);break;
+				case "GREATER": result = Greater(result);break;
+				case "LESSEQUAL": result = LessEqual(result);break;
+				case "GREATEREQUAL": result = GreaterEqual(result);break;
+			}
+		//}
+		try{Console.WriteLine(result.opinion());}catch(NullReferenceException){Console.WriteLine("Rela");}
+		return result;
+	}
+	
+	public Protuberance Less(Protuberance v8Expression){
+		MatchAndEat("LESS");
+		return new Operation("LESS",v8Expression ,ArithmeticExpression());
+	}
+	
+	public Protuberance LessEqual(Protuberance v8Expression){
+		MatchAndEat("LESSEQUAL");
+		return new Operation("LESSEQUAL",v8Expression ,ArithmeticExpression());
+	}
+	
+	public Protuberance Equal(Protuberance v8Expression){
+		MatchAndEat("EQUAL");
+		return new Operation("EQUAL",v8Expression ,ArithmeticExpression());
+	}
+	
+	public Protuberance Greater(Protuberance v8Expression){
+		MatchAndEat("GREATER");
+		return new Operation("GREATER",v8Expression ,ArithmeticExpression());
+	}
+	
+	public Protuberance GreaterEqual(Protuberance v8Expression){
+		MatchAndEat("GREATEREQUAL");
+		return new Operation("GREATEREQUAL",v8Expression ,ArithmeticExpression());
+	}
+	
+	public Protuberance v8Factor(){
+		return Relation();
+	}
+	
+	public Protuberance v8Term(){
+		Protuberance result = v8Factor();
+		while(CurrentToken().type.Equals("AND")){
+			MatchAndEat("AND");
+			result = new Operation("AND",result,v8Factor());
+		}
+		try{Console.WriteLine(result.opinion());}catch(NullReferenceException){Console.WriteLine("v8Term");}
+		return result;
+	}
+	
+	public Protuberance v8Expression(){
+		Protuberance result = v8Term();
+		while(CurrentToken().type.Equals("OR")){
+			MatchAndEat("OR");
+			result = new Operation("OR",result,v8Term());
+		}
+		try{Console.WriteLine(result.opinion());}catch(NullReferenceException){Console.WriteLine("v8Ex");}
+		return result;
+	}
 
     public static void Main() {
 		while(true){
-        String expression = "";
-		Console.Write("Calculator# ");
-		expression = Console.ReadLine();
-        expression += " ";
-		
-        Calculator calc = new Calculator();
-        Tokenizer tokenizer = new Tokenizer();
-        calc.tokens = tokenizer.getTokens(expression);
-		
-        Console.WriteLine("Expression Result: " + calc.ArithmeticExpression());
+			String expression = "";
+			Console.Write("Calculator# ");
+			expression = Console.ReadLine();
+			expression += " ";
+			
+			Calculator calc = new Calculator();
+			Tokenizer tokenizer = new Tokenizer();
+			calc.tokens = tokenizer.getTokens(expression);
+			Protuberance result = calc.v8Expression();
+			try{Console.WriteLine("Expression Result: " + result.opinion());}catch(NullReferenceException){Console.WriteLine("Main");}
 		}
     }
 }
@@ -149,10 +225,11 @@ public class Token {
 public class Tokenizer {
 
     public bool IsOp(char c) {
-        return (c=='+') || (c=='-')|| (c=='*') || (c=='/')|| (c=='%')|| (c=='^')|| (c=='(') || (c==')');
+        return (c=='+') || (c=='-')|| (c=='*') || (c=='/')|| (c=='%')|| (c=='^')
+		||(c=='<')||(c=='>')||(c=='=')||(c=='!')||(c=='|')||(c=='&');
     }
 
-    public String FindOpType(char chr) {
+    public String FindOpType(char chr,char next) {
         String type = "NONE";
         switch (chr) {
             case '+': type = "ADD"; break;
@@ -161,34 +238,78 @@ public class Tokenizer {
             case '/': type = "DIVIDE"; break;
 			case '%': type = "MOD"; break;
 			case '^': type = "POWER"; break;
-            case '(': type = "LEFT_PAREN"; break;
-            case ')':type = "RIGHT_PAREN"; break;
+			case '<': type = "LESS"; 
+					 	if(next=='=') 
+							type = "LESSEQUAL";
+					 	break;
+			case '>': type = "GREATER"; 
+					 	if(next=='=') 
+							type = "GREATEREQUAL";
+					 	break;
+			case '=': type = "ASSIGMENT"; 
+					 	if(next=='=') 
+							type = "EQUAL";
+					 	break;
+			case '!': type = "NOT";
+					 	if(next=='=') 
+							type = "NOTEQUAL";
+					 	break;
+			case '|': type = "OR";break;
+			case '&': type = "AND";break;
         }
         return type;
     }
+	
+	public String FindParentType(char chr){
+		String type = "UNKNOWN";
+		switch(chr){
+			case '(': type = "LEFT_PAREN"; break;
+            case ')': type = "RIGHT_PAREN"; break;
+		}
+		return type;
+	}
 
     public List<Token> getTokens(String source) {
         List<Token> tokens = new List<Token>();
-        String token = "";
+        String tokenString = "";
         String state = "DEFAULT";
+		Token token = null;
+		char v8 = '\0';
         for (int index = 0; index < source.Length; index++) {
             char chr = source[index];
             switch (state) {
                 case "DEFAULT":
-                    String opType = FindOpType(chr);
                     if (IsOp(chr)) {
-                        tokens.Add(new Token((""+chr), opType));
+						v8 = chr;
+						String opType = FindOpType(chr,'\0');
+                        //tokens.Add(new Token((""+chr), opType));
+						token = new Token((""+chr),opType);
+						state = "OPERATOR";
                     } else if (Char.IsDigit(chr)||(chr=='.')) {
-                        token += chr;
+                        tokenString += chr;
                         state = "NUMBER";
-                    }
+                    } else if((chr=='(')||(chr==')')){
+						String par = FindParentType(chr);
+						tokens.Add(new Token((""+chr),par));
+					}
                     break;
+				case "OPERATOR":
+					if(IsOp(chr)){
+						String opType = FindOpType(v8,chr);
+						token = new Token((""+v8+chr),opType);
+					}
+					else{
+						tokens.Add(token);
+						state = "DEFAULT";
+						index--;
+					}
+					break;
                 case "NUMBER":
                     if (Char.IsDigit(chr)||(chr=='.')) {
-                        token += chr;
+                        tokenString += chr;
                     } else {
-                        tokens.Add(new Token(token, "NUMBER"));
-                        token = "";
+                        tokens.Add(new Token(tokenString, "NUMBER"));
+                        tokenString = "";
                         state = "DEFAULT";
                         index--;
                     }
@@ -196,5 +317,132 @@ public class Tokenizer {
             }
         }
         return tokens;
+    }
+}
+
+public abstract class Protuberance{
+	public Protuberance(){}
+	public abstract Object opinion();
+}
+
+public class Number : Protuberance {
+    double value;
+	
+    public Number() {}
+	
+    public Number(double value) {
+        this.value = value;
+    }
+	
+    public override Object opinion() {
+        return value;
+    }
+	
+    public String toString() {
+        return value + "";
+    }
+}
+
+
+public class Boolean : Protuberance {
+
+    bool value;
+
+    public Boolean() {
+    }
+
+    public Boolean(bool value) {
+        this.value = value;
+    }
+
+    public override Object opinion() {
+        return value;
+    }
+
+    public String toString() {
+        return value + "";
+    }
+}
+
+public class Operation : Protuberance {
+
+    public String op;
+    public Protuberance left;
+    public Protuberance right;
+
+    public Operation() {
+    }
+
+    public Operation(String op, Protuberance left, Protuberance right) {
+        this.op = op;
+        this.left = left;
+        this.right = right;
+    }
+
+    public double ToDouble(Protuberance p) {
+        Object result = p.opinion();
+		double r = 0;
+		try{
+		r = Double.Parse(result.ToString());
+		}catch(FormatException){Console.WriteLine("Aqui");}
+        return r;
+    }
+
+    public bool ToBoolean(Protuberance p) {
+        Object result = p.opinion();
+        return Convert.ToBoolean(result);
+    }
+
+    public Object ToObject(Protuberance p) {
+        return p.opinion();
+    }
+
+    public override Object opinion() {
+        Object result = null;
+        switch (op) {
+            case "ADD":
+                result = (Object)(ToDouble(left) + ToDouble(right));
+                break;
+            case "SUBTRACT":
+                result = (Object)(ToDouble(left) - ToDouble(right));
+                break;
+            case "MULTIPLY":
+                result = (Object)(ToDouble(left) * ToDouble(right));
+                break;
+            case "DIVIDE":
+                if (ToDouble(right) == 0) {
+                    Console.WriteLine("Error: Division by Zero!");
+                    System.Environment.Exit(1);
+                }
+                result = (Object)(ToDouble(left) / ToDouble(right));
+                break;
+			case "MOD": result = (Object)(ToDouble(left)%ToDouble(right));break;
+			case "POWER": result = (Object)(Math.Pow(ToDouble(left),ToDouble(right)));break;
+            case "LESS":
+                result = Convert.ToBoolean(ToDouble(left) < ToDouble(right));
+                break;
+            case "GREATER":
+                result = Convert.ToBoolean(ToDouble(left) > ToDouble(right));
+                break;
+            case "EQUAL":
+                result = Convert.ToBoolean(ToObject(left).Equals(ToObject(right)));
+                break;
+            case "NOTEQUAL":
+                result = Convert.ToBoolean(!ToObject(left).Equals(ToObject(right)));
+                break;
+            case "LESSEQUAL":
+                result = Convert.ToBoolean(ToDouble(left) <= ToDouble(right));
+                break;
+            case "GREATEREQUAL":
+                result = Convert.ToBoolean(ToDouble(left) >= ToDouble(right));
+                break;
+            case "OR":
+                result = Convert.ToBoolean(ToBoolean(left) || ToBoolean(right));
+                break;
+            case "AND":
+                result = Convert.ToBoolean(ToBoolean(left) && ToBoolean(right));
+                break;
+        }
+        return result;
     }
 }
